78156db43b6e53cb7e37a0a2655c5b3fc8e10886
Adem Halici <ademhalici4@gmail.com>
2025-08-11 20:10:02 +1000

    added datasets and dataloaders

--- test.py
+++ test.py
@@ -1,3 +1,32 @@
 import torch
-x = torch.rand(5, 3)
-print(x)
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Downloads training data
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Downloads test data
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+chosen_batch_size = 64
+
+# Creates data loaders
+train_dataloader = DataLoader(training_data, batch_size = chosen_batch_size)
+test_dataloader = DataLoader(test_data, batch_size = chosen_batch_size)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
