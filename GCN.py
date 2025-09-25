db28208b7793f1ef0ae84eb13daa63ae6f89fa20
Adem Halici <ademhalici4@gmail.com>
2025-09-26 02:44:22 +1000

    assistance with GCN representation

--- GCN.py
+++ GCN.py
@@ -8,61 +8,68 @@ from torch_geometric.nn import global_mean_pool, GCN
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 
-import pickle
-
-"""
-#create example data for graph, this will have to be switched out with a dataLoader
-types = [0, 1, 2]
-pos = [0, 2, 1]
-edges = [(0, 1), (1, 2)]
-y = 0.5
-
-#creates graph object, probably part of the dataLoader process?
-g = Graph(directed=True)
-g.add_vertices(3)
-g.vs['type'] = types
-g.vs['pos'] = pos
-g.add_edges(edges)
-
-#print out feature information for each node
-for v in g.vs:
-    print(v.attributes())
-
-#print out adjacency matrix, note that because directed graph, only 2 adjacencies
-A = g.get_adjacency()
-print(A)
-
-#print image rep of graph at position 0
-igraph.plot(g, 'testGraph.png', vertex_label=types, vertex_size=30)
-"""
+import pprint, pickle
 
 #import OCBdataset pickle file and load into variable, creates tuple of igraph.Graph objects
-OCBdataset = pickle.load(open(".\data\OCBdataset\ckt_bench_101.pkl", 'rb'))
+OCBdataset = pickle.load(open(r"data\OCBdataset\ckt_bench_101.pkl", 'rb'))
+OCBtensorset = pickle.load(open(r"data\OCBdataset\ckt_bench_101_tensor.pkl", 'rb'))
+OCBpygraphset = pickle.load(open(r"data\OCBdataset\ckt_bench_101_pygraph.pkl", 'rb'))
+OCBtestset = open(r"data\OCBdataset\perform101.csv", 'rb')
 
 #create GraphDataset object and define the constructor method
 class GraphDataset(Data):
-    def __init__(self, types=None, pos=None, edge_index=None, y=None):
+    def __init__(self, c=None, gm=None, pos=None, r=None, type=None, vid=None, edge_index=None, y=None):
         super().__init__(edge_index=edge_index, y=y)
-        self.types = types
+        self.c = c
+        self.gm = gm
         self.pos = pos
+        self.r = r
+        self.type = type
+        self.vid = vid
+
+trainingDataset = OCBdataset[0]
+testingDataset = OCBdataset[1]
 
 #create array and tensorise graph information to create Data object to place in array
-G = []
-types = torch.tensor(g.vs['type'])
-pos = torch.tensor(g.vs['pos'])
-y = torch.tensor([y], dtype=torch.float32)
-edge_index = torch.tensor(g.get_edgelist(), dtype=torch.long).permute(1, 0)
-#print(edge_index)
+gTrain = []
+gTest = []
 
-#creating the first batch?
-g_pyg = GraphDataset(types=types, pos=pos, edge_index=edge_index, y=y)
-G.append(g_pyg)
+for richGraph, simpleGraph in trainingDataset:
+    c = torch.tensor(richGraph.vs['c'])
+    gm = torch.tensor(richGraph.vs['gm'])
+    pos = torch.tensor(richGraph.vs['pos'])
+    r = torch.tensor(richGraph.vs['r'])
+    type = torch.tensor(richGraph.vs['type'])
+    vid = torch.tensor(richGraph.vs['vid'])
+    edge_index = torch.tensor(richGraph.get_edgelist(), dtype=torch.long).permute(1, 0)
+    y = torch.tensor([richGraph.vcount()], dtype=torch.float32)
+    richGraph_pyg = GraphDataset(c=c, gm=gm, pos=pos, r=r, type=type, vid=vid, edge_index=edge_index, y=y)
+    gTrain.append(richGraph_pyg)
 
-#creating the second batch?
-g_pyg2 = GraphDataset(types=types, pos=pos, edge_index=edge_index, y=y)
-G.append(g_pyg2)
+for richGraph, simpleGraph in testingDataset:
+    c = torch.tensor(richGraph.vs['c'])
+    gm = torch.tensor(richGraph.vs['gm'])
+    pos = torch.tensor(richGraph.vs['pos'])
+    r = torch.tensor(richGraph.vs['r'])    
+    type = torch.tensor(richGraph.vs['type'])
+    vid = torch.tensor(richGraph.vs['vid'])
+    edge_index = torch.tensor(richGraph.get_edgelist(), dtype=torch.long).permute(1, 0)
+    y = torch.tensor([richGraph.vcount()], dtype=torch.float32)
+    richGraph_pyg = GraphDataset(c=c, gm=gm, pos=pos, r=r, type=type, vid=vid, edge_index=edge_index, y=y)
+    gTest.append(richGraph_pyg)
 
-dataloader = DataLoader(G, batch_size=2)
+
+trainDataloader = DataLoader(gTrain, batch_size=2)
+testDataloader = DataLoader(gTest, batch_size=2)
+
+#debugging check 
+for batch in trainDataloader:
+    print(batch)
+    print("Node feature shape:", batch.x.shape if batch.x is not None else "None")
+    print("Edge index shape:", batch.edge_index.shape)
+    print("Target shape:", batch.y.shape)
+    break
+
 
 #create Model object and define constructor method
 class Model(nn.Module):
@@ -75,18 +82,14 @@ class Model(nn.Module):
     
     #define forward method
     def forward(self, batch_data, device):
-        x_t = batch_data.types
-        x_p = batch_data.pos
+        x_t = F.one_hot(batch_data.type, num_classes=14).to(torch.float32)
+        x_num = torch.stack([c, gm, pos, r], dim=-1).to(torch.float32)
         e = batch_data.edge_index
 
-        #One-hot Embedding
-        x_t = F.one_hot(x_t, num_classes=3).to(torch.float32)
-        x_p = F.one_hot(x_p, num_classes=3).to(torch.float32)
-
         x_t = self.linear1(x_t)  #(B*N, dx)
-        x_p = self.linear2(x_p)  #(B*N, dx)
+        x_num = self.linear2(x_num)  #(B*N, dx)
 
-        z = torch.cat((x_t, x_p), dim=-1)  #(B, N, dx*2)
+        z = torch.cat((x_t, x_num), dim=-1)  #(B, N, dx*2)
 
         z = self.gcn(z, e) #(B*N, dx)
 
@@ -102,14 +105,12 @@ num_layers = 3
 # If you run under Nvidia GPU have cuda installed, you could accelerate the computation through GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Model(input_dim, hidden_dim, num_layers).to(device)
-model.train()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 criterion = nn.MSELoss()
 
-"""
 for i in range(0, epochs):
     print(f"------------------ epoch_{i} -----------------")
-    for i, batch_data in enumerate(dataloader):
+    for i, batch_data in enumerate(trainDataloader):
         batch_data.to(device)
         optimizer.zero_grad()
         pred = model(batch_data, device)
@@ -117,4 +118,3 @@ for i in range(0, epochs):
         loss.backward()
         optimizer.step()
         print(loss.item())
-"""
